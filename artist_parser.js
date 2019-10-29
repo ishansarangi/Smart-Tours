d1afd20f16d96808420b76e69f419c4f0bff0ebd
Sabyasachi <mohantysabyasachi27@gmail.com>
2019-10-28 22:08:02 -0700

    Smart Tours - Convert into a node project

---
+++ artist_parser.js
@@ -0,0 +1,42 @@
+const csv = require('fast-csv');
+
+var result_arr = [];
+
+var temp = [];
+    temp.push("ConstituentID");
+    temp.push("DisplayName");
+    temp.push("ArtistBio");
+    temp.push("Nationality");
+    temp.push("Gender");
+    temp.push("BeginDate");
+    temp.push("EndDate");
+    temp.push("Wiki_QID");
+    temp.push("ULAN");
+
+    result_arr.push(temp);
+
+csv.parseFile('./Artists.csv', {headers: true})
+.on('data', data => {
+
+  if(data.ConstituentID != '' && data.DisplayName != '' && data.ArtistBio != '' && data.Nationality != '' &&
+    data.Gender != '' && data.BeginDate != 0 && data.EndDate != 0 && data.Wiki_QID != '' && data.ULAN != ''){
+    var temp = [];
+    temp.push(data.ConstituentID);
+    temp.push(data.DisplayName);
+    temp.push(data.ArtistBio);
+    temp.push(data.Nationality);
+    temp.push(data.Gender);
+    temp.push(data.BeginDate);
+    temp.push(data.EndDate);
+    temp.push(data.Wiki_QID);
+    temp.push(data.ULAN);
+
+    result_arr.push(temp);
+  }
+
+})
+.on("end", () => {
+  csv.writeToPath('./Artist_result.csv', result_arr);
+});
+
+
